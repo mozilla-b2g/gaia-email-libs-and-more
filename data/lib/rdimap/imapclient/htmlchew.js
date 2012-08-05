b4d0096e58d9e8134cdb155a002c6cf4c5d56097
Andrew Sutherland <asutherland@asutherland.org>
2012-08-05 12:03:33 -0700

    sanitization changes

--- data/lib/rdimap/imapclient/htmlchew.js
+++ data/lib/rdimap/imapclient/htmlchew.js
@@ -27,55 +27,139 @@ define(
  * Whitelisted HTML tags list. Currently from nsTreeSanitizer.cpp which credits
  * Mark Pilgrim and Sam Ruby for its own initial whitelist.
  *
+ * Some things are just not in the list at all:
+ * - SVG: Thunderbird nukes these itself because it forces
+ *   SanitizerCidEmbedsOnly which causes flattening of everything in the SVG
+ *   namespace.
+ *
  * Tags that we are opting not to include will be commented with a reason tag:
  * - annoying: This thing is ruled out currently because it only allows annoying
- *   things to happen.
+ *   things to happen *given our current capabilities*.
  * - scripty: This thing requires scripting to make anything happen, and we do
  *   not allow scripting.
+ * - forms: Thunderbird explicitly and unconditionally forbids forms, presumably
+ *   because they are useless locally without scripting, and their target is
+ *   is not reflected in the UI like hyperlinks are or at least should be.
  * - implicitly-nuked: killed as part of the parse process because we assign
  *   to innerHTML rather than creating a document with the string in it.
- * - inline-style-only:
+ * - inline-style-only: Styles have to be included in the document itself,
+ *   and for now, on the elements themselves.  We'll support <style> tags at
+ *   some point.
+ * - dangerous: The semantics of the tag are intentionally at odds with our
+ *   goals and/or are extensible.  (ex: link tag.)
+ * - interactive-ui: A cross between scripty and forms, things like (HTML5)
+ *   menu and command imply some type of mutation that requires scripting.
+ *   They also are frequently very attribute-heavy.
+ * - non-css-presentation: It's a styling thing that's not CSS.  Apparently
+ *   Thunderbird's original sanitizer didn't let this kind of stuff through.
+ *   Now that nsTreeSanitizer is used, SanitizerDropNonCSSPresentation is
+ *   specified by default.
  */
 var LEGAL_TAGS = [
   'a', 'abbr', 'acronym', 'area', 'article', 'aside',
   // annoying: 'audio',
-  'b', 'bdi', 'bdo', 'big', 'blockquote',
+  'b',
+  'bdi', 'bdo', // (bidirectional markup stuff)
+  'big', 'blockquote',
   // implicitly-nuked: 'body'
-  'br', 'button',
+  'br',
+  // forms: 'button',
   // scripty: canvas
-  'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'command',
-  'datalist', 'dd', 'del', 'details', 'dfn', 'dir', 'div', 'dl', 'dt',
-  'em', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form',
+  'caption',
+  // non-css-presentation: 'center',
+  'cite', 'code', 'col', 'colgroup',
+  // interactive-ui: 'command',
+  // forms: 'datalist',
+  'dd', 'del', 'details', 'dfn', 'dir', 'div', 'dl', 'dt',
+  'em',
+  // forms: 'fieldset' (but allowed by nsTreeSanitizer)
+  'figcaption', 'figure',
+  // non-css-presentation: 'font',
+  'footer',
+  // forms: 'form',
   'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
   // implicitly-nuked: head
   'header', 'hgroup', 'hr',
   // implicitly-nuked: html
-  'i', 'img', 'input', 'ins',
-  'kbd',
-  'label', 'legend', 'li', 'link', 'listing',
-  'map', 'mark', 'menu', 'meta', 'meter',
-  'nav', 'nobr', 'noscript',
-  'ol', 'optgroup', 'option', 'output',
-  'p', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby',
-  's', 'samp', 'section', 'select', 'small',
+  'i', 'img',
+  // forms: 'input',
+  'ins', // ("represents a range of text that has been inserted to a document")
+  'kbd', // ("The kbd element represents user input")
+  'label', 'legend', 'li',
+  // dangerous, inline-style-only: link
+  /* link supports many types, none of which we want, some of which are
+   * risky: http://dev.w3.org/html5/spec/links.html#linkTypes. Specifics:
+   * - "stylesheet": This would be okay for cid links, but there's no clear
+   *   advantage over inline styles, so we forbid it, especially as supporting
+   *   it might encourage other implementations to dangerously support link.
+   * - "prefetch": Its whole point is de facto information leakage.
+   */
+  'listing', // (deprecated, like "pre")
+  'map', 'mark',
+  // interactive-ui: 'menu', 'meta', 'meter',
+  'nav',
+  'nobr', // (deprecated "white-space:nowrap" equivalent)
+  'noscript',
+  'ol',
+  // forms: 'optgroup',
+  // forms: 'option',
+  'output', // (HTML5 draft: "result of a calculation in a form")
+  'p', 'pre',
+  // interactive-ui: 'progress',
+  'q',
+  /* http://www.w3.org/TR/ruby/ is a pronounciation markup that is not directly
+   * supported by gecko at this time (although there is a Firefox extension).
+   * All of 'rp', 'rt', and 'ruby' are ruby tags.  The spec also defines 'rb'
+   * and 'rbc' tags that nsTreeSanitizer does not whitelist, however.
+   */
+  'rp', 'rt', 'ruby',
+  's', 'samp', 'section',
+  // forms: 'select',
+  'small',
   // annoying?: 'source',
   'span', 'strike', 'strong',
   // inline-style-only: 'style'
   'sub', 'summary', 'sup',
-  'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time',
+  'table', 'tbody', 'td',
+  // forms: 'textarea',
+  'tfoot', 'th', 'thead', 'time',
   'title', // XXX does this mean anything outside head?
   'tr',
   // annoying?: 'track'
   'tt',
   'u', 'ul', 'var',
   // annoying: 'video',
-  'wbr'
+  'wbr' // (HTML5 draft: line break opportunity)
 ];
 
+/**
+ * Tags whose children should be removed along with the tag itself, rather than
+ * splicing the children into the position originally occupied by the parent.
+ *
+ * We do this for:
+ * - forms; see `LEGAL_TAGS` for the rationale.  Note that we don't bother
+ *   including children that should already be nuked by PRUNE_TAGS.  For
+ *   example, 'option' and 'optgroup' only make sense under 'select' or
+ *   'datalist', so we need not include them.  This means that if the tags
+ *   are used in nonsensical positions, they will have their contents
+ *   merged into the document text, but that's not a major concern.
+ * - 'script': no one wants to read the ignored JS code!
+ * - 'style': no one wants to read the CSS we are (currently) ignoring
+ */
+var PRUNE_TAGS = [
+  'button',
+  'datalist',
+  'select',
+];
+
+/**
+ * What attributes to allow globally and on specific tags.
+ */
 var LEGAL_ATTR_MAP = {
   '*': ['style'],
+  'a': ['href'],
+  'area': ['href'],
   'img': ['alt'],
-
 };
 
 var LEGAL_STYLES = [
@@ -86,9 +170,10 @@ var LEGAL_STYLES = [
 
 var BLEACH_SETTINGS = {
   tags: LEGAL_TAGS,
+  prune: PRUNE_TAGS,
   attributes: LEGAL_ATTR_MAP,
   styles: LEGAL_STYLES,
-},
+};
 
 /**
  * @args[
