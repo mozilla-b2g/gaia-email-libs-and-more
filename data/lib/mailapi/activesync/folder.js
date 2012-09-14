5a71b9377290ccc19b13641f53d7b3d1ea138aba
Jim Porter <jporter@mozilla.com>
2012-09-13 22:05:42 -0500

    Support getting more than 100 messages per folder for ActiveSync

--- data/lib/mailapi/activesync/folder.js
+++ data/lib/mailapi/activesync/folder.js
@@ -171,6 +171,7 @@ ActiveSyncFolderConn.prototype = {
       let changed = [];
       let deleted = [];
       let status;
+      let moreAvailable = false;
 
       folderConn._account._syncsInProgress--;
 
@@ -192,12 +193,16 @@ ActiveSyncFolderConn.prototype = {
       let e = new $wbxml.EventParser();
       const base = [as.Sync, as.Collections, as.Collection];
 
+      e.addEventListener(base.concat(as.SyncKey), function(node) {
+        folderConn.syncKey = node.children[0].textContent;
+      });
+
       e.addEventListener(base.concat(as.Status), function(node) {
         status = node.children[0].textContent;
       });
 
-      e.addEventListener(base.concat(as.SyncKey), function(node) {
-        folderConn.syncKey = node.children[0].textContent;
+      e.addEventListener(base.concat(as.MoreAvailable), function(node) {
+        moreAvailable = true;
       });
 
       e.addEventListener(base.concat(as.Commands, [[as.Add, as.Change]]),
@@ -243,7 +248,9 @@ ActiveSyncFolderConn.prototype = {
       if (status === asEnum.Status.Success) {
         console.log('Sync completed: added ' + added.length + ', changed ' +
                     changed.length + ', deleted ' + deleted.length);
-        callback(null, added, changed, deleted);
+        callback(null, added, changed, deleted, moreAvailable);
+        if (moreAvailable)
+          folderConn._enumerateFolderChanges(callback);
       }
       else if (status === asEnum.Status.InvalidSyncKey) {
         console.warn('ActiveSync had a bad sync key');
@@ -440,9 +447,11 @@ ActiveSyncFolderConn.prototype = {
                                              doneCallback) {
     let storage = this._storage;
     let folderConn = this;
+    let messagesSeen = 0;
 
     this._LOG.syncDateRange_begin(null, null, null, startTS, endTS);
-    this._enumerateFolderChanges(function (error, added, changed, deleted) {
+    this._enumerateFolderChanges(function (error, added, changed, deleted,
+                                           moreAvailable) {
       if (error === 'badkey') {
         folderConn._account._recreateFolder(storage.folderId, function(s) {
           folderConn.storage = s;
@@ -468,12 +477,13 @@ ActiveSyncFolderConn.prototype = {
         storage.deleteMessageByUid(messageGuid);
       }
 
-      // XXX: We should check for <AirSync:MoreAvailable/> and let the folder
-      // storage know, or more likely, just keep grabbing headers.
+      messagesSeen += added.length + changed.length + deleted.length;
 
-      folderConn._LOG.syncDateRange_end(null, null, null, startTS, endTS);
-      storage.markSyncRange(startTS, endTS, 'XXX', accuracyStamp);
-      doneCallback(null, added.length);
+      if (!moreAvailable) {
+        folderConn._LOG.syncDateRange_end(null, null, null, startTS, endTS);
+        storage.markSyncRange(startTS, endTS, 'XXX', accuracyStamp);
+        doneCallback(null, messagesSeen);
+      }
     });
   },
 };
