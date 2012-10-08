b5ab06fcb6e7c67ff2a08deab04969a5282b1cfa
Andrew Sutherland <asutherland@asutherland.org>
2012-10-08 00:06:10 -0400

    test_imap_just_auth now passes for yahoo.com with my activesync changes
    The ActiveSync changes are in a pull request to jsas; waiting on that
    to update the dep in here to avoid submodule snafus.

--- test/unit/resources/window_shims.js
+++ test/unit/resources/window_shims.js
@@ -77,6 +77,18 @@ var _window_mixin = {
     parser.init();
     return parser;
   },
+  XPathResult: {
+    ANY_TYPE: 0,
+    NUMBER_TYPE: 1,
+    STRING_TYPE: 2,
+    BOOLEAN_TYPE: 3,
+    UNORDERED_NODE_ITERATOR_TYPE: 4,
+    ORDERED_NODE_ITERATOR_TYPE: 5,
+    UNORDERED_NODE_SNAPSHOT_TYPE: 6,
+    ORDERED_NODE_SNAPSHOT_TYPE: 7,
+    ANY_UNORDERED_NODE_TYPE: 8,
+    FIRST_ORDERED_NODE_TYPE: 9
+  },
 
   navigator: {
     // - connection status
