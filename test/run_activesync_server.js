e886267406de837232e4dea01dcceafa4d3d287d
Jim Porter <jporter@mozilla.com>
2012-10-25 18:13:18 -0500

    [activesync/fakeserver] Use the port defined in the makefile

--- test/run_activesync_server.js
+++ test/run_activesync_server.js
@@ -1,8 +1,18 @@
+/**
+ * This file runs a standalone ActiveSync server.
+ *
+ * It is meant to be executed with an xpcshell.
+ *
+ * The Makefile in the root directory contains a target to run it:
+ *
+ *   $ make activesync-server
+ */
+
 'use strict';
 
 load('test/activesync_server.js');
 
 let server = new ActiveSyncServer();
-server.start(8080);
+server.start(SERVER_PORT);
 
 _do_main();
