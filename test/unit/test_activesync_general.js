c3d2e4864efd38c6d6596fe3fe39cbcba8f072cf
Jim Porter <jporter@mozilla.com>
2012-10-26 18:53:16 -0500

    [activesync/fakeserver] Fix test comments

--- test/unit/test_activesync_general.js
+++ test/unit/test_activesync_general.js
@@ -1,20 +1,7 @@
 /**
- * Test our IMAP sync logic under non-pathological conditions.  We exercise
- * logic with a reasonable number of messages.  Test cases that involve folders
- * with ridiculous numbers of messages and/or huge gaps in time which will take
- * a while to run and/or might upset the IMAP servers are/will be handled by
- * `test_imap_complex.js`.
- *
- * Our tests verify that:
- * - Live synchronization provides the expected results where the messages come
- *   direct from the connection as they are added.
- * - Listing the messages in an already synchronized folder when offline
- *   properly retrieves the messages.
- * - Resynchronizing an already-synchronized unchanged folder only issues
- *   the time-range search and flag fetches and returns the same set of
- *   messages.
- * - Resynchronizing an already-synchronized folder detects new messages,
- *   deleted messages, and flag changes.
+ * Test our ActiveSync sync logic under non-pathological conditions.  Currently,
+ * this just tests that we can create an account successfully.  More tests
+ * coming soon!
  **/
 
 load('resources/loggest_test_framework.js');
