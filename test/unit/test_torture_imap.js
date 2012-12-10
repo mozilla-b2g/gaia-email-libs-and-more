3b7a14d7f550bed29a45c84d2ea6eebf2656f0b7
Andrew Sutherland <asutherland@asutherland.org>
2012-12-10 13:30:56 -0500

    add forgotten files

---
+++ test/unit/test_torture_imap.js
@@ -0,0 +1,46 @@
+/**
+ * This creates message loads that generate overload conditions for IMAP using
+ * our default constants.
+ */
+
+load('resources/loggest_test_framework.js');
+
+var TD = $tc.defineTestsFor(
+  { id: 'test_torture_imap' }, null, [$th_imap.TESTHELPER], ['app']);
+
+TD.commonCase('obliterate', function(T) {
+  T.group('setup');
+  var testUniverse = T.actor('testUniverse', 'U', { stockDefaults: true }),
+      testAccount = T.actor('testAccount', 'A', { universe: testUniverse }),
+      eSync = T.lazyLogger('sync');
+
+  const FILL_SIZE = 60;
+  testUniverse.do_adjustSyncValues({
+    fillSize: FILL_SIZE,
+  });
+
+  var testFolder = testAccount.do_createTestFolder(
+    'test_torture',
+    { count: FILL_SIZE * 20, age: { days: 0 }, age_incr: { days: 1 },
+      age_incr_every: FILL_SIZE * 5 });
+  testAccount.do_viewFolder(
+    'syncs', testFolder,
+    // bisect case, all nulls
+    [{ count: null, full: null, flags: null, deleted: null },
+    // a full day (300) will be fetched, but only 15 returned of course
+     { count: FILL_SIZE, full: FILL_SIZE * 5, flags: 0, deleted: 0 }],
+    { top: true, bottom: false, grow: false },
+    {
+      expectFunc: function() {
+        // This will generate so much write traffic that a purgeExcessMessages
+        // job will be scheduled and run after the sync.
+        testAccount.expect_runOp('purgeExcessMessages', false);
+        testFolder.storageActor.expect_mutexedCall_begin('purgeExcessMessages');
+        testFolder.storageActor.expect_mutexedCall_end('purgeExcessMessages');
+      }
+    }).timeoutMS = 5 * 1000;
+});
+
+function run_test() {
+  runMyTests(90); // we do a lot of appending...
+}
