b530b7c182d058e5509a0dc7ac23ff920764fd9d
Andrew Sutherland <asutherland@asutherland.org>
2013-04-01 03:22:21 -0400

    Bug 846373 - [email] URL/domain name/http/https/e-mail address/mailto link detection/linkification process is very slow

--- data/lib/mailapi/mailapi.js
+++ data/lib/mailapi/mailapi.js
@@ -1192,6 +1192,64 @@ var unexpectedBridgeDataError = reportError,
     internalError = reportError,
     reportClientCodeError = reportError;
 
+
+// Common idioms:
+//
+// Lead-in (URL and email):
+// (                     Capture because we need to know if there was a lead-in
+//                       character so we can include it as part of the text
+//                       preceding the match.  We lack look-behind matching.
+//  ^|                   The URL/email can start at the beginninf of the string.
+//  [\s(,;]              Or whitespace or some punctuation that does not imply
+//                       a context which would preclude a URL.
+// )
+//
+// We do not need a trailing look-ahead because our regex's will terminate
+// because they run out of characters they can eat.
+
+// What we do not attempt to have the regexp do:
+// - Avoid trailing '.' and ')' characters.  We let our greedy match absorb
+//   these, but have a separate regex for extra characters to leave off at the
+//   end.
+//
+// The Regex (apart from lead-in/lead-out):
+// (                     Begin capture of the URL
+//  (?:                  (potential detect beginnings)
+//   https?:\/\/|        Start with "http" or "https"
+//   www\d{0,3}[.][a-z0-9.\-]{2,249}|
+//                      Start with "www", up to 3 numbers, then "." then
+//                       something that looks domain-namey.  We differ from the
+//                       next case in that we do not constrain the top-level
+//                       domain as tightly and do not require a trailing path
+//                       indicator of "/".
+//   [a-z0-9.\-]{2,250}[.][a-z]{2,4}\/
+//                       Detect a non-www domain, but requiring a trailing "/"
+//                       to indicate a path.
+//
+//                       Domain names can be up to 253 characters long, and are
+//                       limited to a-zA-Z0-9 and '-'.  The roots don't have
+//                       hyphens.
+//  )
+//  [-\w.!~*'();,/?:@&=+$#%]*
+//                       path onwards. We allow the set of characters that
+//                       encodeURI does not escape plus the result of escaping
+//                       (so also '%')
+// )
+var RE_URL =
+  /(^|[\s(,;])((?:https?:\/\/|www\d{0,3}[.][a-z0-9.\-]{2,249}|[a-z0-9.\-]{2,250}[.][a-z]{2,4}\/)[-\w.!~*'();,/?:@&=+$#%]*)/im;
+// Set of terminators that are likely to have been part of the context rather
+// than part of the URL and so should be uneaten.  This is the same as our
+// mirror lead-in set (so '(', ',', ';') plus question end-ing punctuation and
+// the potential permutations with parentheses (english-specific)
+var RE_UNEAT_LAST_URL_CHARS = /(?:[),;.!?]|[.!?]\)|\)[.!?])$/;
+// Don't require the trailing slashes here for pre-pending purposes, although
+// our above regex currently requires them.
+var RE_HTTP = /^https?:/i;
+// Note: the [^\s] is fairly international friendly, but might be too friendly.
+var RE_MAIL =
+  /(^|[\s(,;])([^(,;@\s]+@[^.\s]+.[a-z]+)/m;
+var RE_MAILTO = /^mailto:/i;
+
 var MailUtils = {
 
   /**
@@ -1199,44 +1257,55 @@ var MailUtils = {
    */
   linkifyPlain: function(body, doc) {
     var nodes = [];
-    var match = true;
+    var match = true, contentStart;
     while (true) {
-      var url =
-        body.match(/^([\s\S]*?)(^|\s)((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/im);
-      var email =
-        body.match(/^([\s\S]*?)(^|\s)([^@\s]+@[^.\s]+.[a-z]+)($|\s)/m);
+      var url = RE_URL.exec(body);
+      var email = RE_MAIL.exec(body);
       // Pick the regexp with the earlier content; index will always be zero.
       if (url &&
-          (!email || url[1].length < email[1].length)) {
-        var first = url[1] + url[2];
-        if (first.length > 0)
-          nodes.push(doc.createTextNode(first));
+          (!email || url.index < email.index)) {
+        contentStart = url.index + url[1].length;
+        if (contentStart > 0)
+          nodes.push(doc.createTextNode(body.substring(0, contentStart)));
+
+        // There are some final characters for a URL that are much more likely
+        // to have been part of the enclosing text rather than the end of the
+        // URL.
+        var useUrl = url[2];
+        var uneat = RE_UNEAT_LAST_URL_CHARS.exec(useUrl);
+        if (uneat) {
+          useUrl = useUrl.substring(0, uneat.index);
+        }
 
         var link = doc.createElement('a');
         link.className = 'moz-external-link';
-        link.setAttribute('ext-href', url[3]);
-        var text = doc.createTextNode(url[3]);
+        // the browser app needs us to put a protocol on the front
+        if (RE_HTTP.test(url[2]))
+          link.setAttribute('ext-href', useUrl);
+        else
+          link.setAttribute('ext-href', 'http://' + useUrl);
+        var text = doc.createTextNode(useUrl);
         link.appendChild(text);
         nodes.push(link);
 
-        body = body.substring(url[0].length);
+        body = body.substring(url.index + url[1].length + useUrl.length);
       }
       else if (email) {
-        first = email[1] + email[2];
-        if (first.length > 0)
-          nodes.push(doc.createTextNode(first));
+        contentStart = email.index + email[1].length;
+        if (contentStart > 0)
+          nodes.push(doc.createTextNode(body.substring(0, contentStart)));
 
         link = doc.createElement('a');
         link.className = 'moz-external-link';
-        if (/^mailto:/.test(email[3]))
-          link.setAttribute('ext-href', email[3]);
+        if (RE_MAILTO.test(email[2]))
+          link.setAttribute('ext-href', email[2]);
         else
-          link.setAttribute('ext-href', 'mailto:' + email[3]);
-        text = doc.createTextNode(email[3]);
+          link.setAttribute('ext-href', 'mailto:' + email[2]);
+        text = doc.createTextNode(email[2]);
         link.appendChild(text);
         nodes.push(link);
 
-        body = body.substring(email[0].length - email[4].length);
+        body = body.substring(email.index + email[0].length);
       }
       else {
         break;
