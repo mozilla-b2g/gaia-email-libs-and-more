09c076b978b3f04f6ee46b696f46fe12a2eb6ba4
Andrew Sutherland <asutherland@asutherland.org>
2013-06-04 23:17:24 -0400

    Bug 879511 - [email/IMAP] date-time parsing regex does not accept legal "SP DIGIT" syntax
    also removes speculative parseImapDate logic function that is not used.

--- test/unit/test_imap_proto.js
+++ test/unit/test_imap_proto.js
@@ -3,14 +3,14 @@
  * the modified utf-7 decoding test.
  */
 
-define(['rdcommon/testcontext', './resources/th_main', 'exports'],
-       function($tc, $th_imap, exports) {
+define(['rdcommon/testcontext', './resources/th_main', 'imap', 'exports'],
+       function($tc, $th_imap, $imap, exports) {
 
 var TD = exports.TD = $tc.defineTestsFor(
   { id: 'test_imap_proto' }, null, [$th_imap.TESTHELPER], ['app']);
 
 TD.commonSimple('decodeModifiedUtf7', function(lazy) {
-  var decodeModifiedUtf7 = require('imap').decodeModifiedUtf7;
+  var decodeModifiedUtf7 = $imap.decodeModifiedUtf7;
 
   function check(encoded, expected) {
     lazy.expect_namedValue(encoded, expected);
@@ -25,6 +25,29 @@ TD.commonSimple('decodeModifiedUtf7', function(lazy) {
   // from RFC3501
   check('~peter/mail/&U,BTFw-/&ZeVnLIqe-',
         '~peter/mail/\u53f0\u5317/\u65e5\u672c\u8a9e');
+});
+
+TD.commonSimple('parseImapDateTime', function(lazy) {
+  function check(str, expectedTimestamp) {
+    var parsedTS = $imap.parseImapDateTime(str);
+
+    lazy.expect_namedValue(str, expectedTimestamp);
+    lazy.namedValue(str, parsedTS);
+  }
+
+  // remember! dates are zero-based!
+
+  // handle "space digit" for day number
+  check(' 4-Jun-2013 14:15:49 -0500',
+        Date.UTC(2013, 5, 4, 19, 15, 49));
+  // handle "digit" for day number (we may get trimmed)
+  check('4-Jun-2013 14:15:49 -0500',
+        Date.UTC(2013, 5, 4, 19, 15, 49));
+  // handle digit digit
+  check('04-Jun-2013 14:15:49 -0500',
+        Date.UTC(2013, 5, 4, 19, 15, 49));
+  check('14-Jun-2013 14:15:49 -0500',
+        Date.UTC(2013, 5, 14, 19, 15, 49));
 });
 
 }); // end define
