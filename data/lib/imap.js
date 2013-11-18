c7e5f8c7b526f7349036cf2669dbc510134640bc
Marcus Cavanaugh <m@mcav.com>
2013-11-18 10:25:50 -0800

    Bug 933079 - [e-mail] gmail connections not noticing new messages properly, suspect gmail IMAP caching behaviour

--- data/lib/imap.js
+++ data/lib/imap.js
@@ -1116,6 +1116,10 @@ ImapConnection.prototype.delBox = function(name, cb) {
   this._send('DELETE', ' "' + escape(name) + '"', cb);
 };
 
+ImapConnection.prototype.noop = function(cb) {
+  this._send('NOOP', '', cb);
+};
+
 ImapConnection.prototype.renameBox = function(oldname, newname, cb) {
   cb = arguments[arguments.length-1];
   if (typeof oldname !== 'string' || oldname.length === 0)
