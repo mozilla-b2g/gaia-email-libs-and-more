9df3b4410b5419deec2a91c310816d8134f0836b
Andrew Sutherland <asutherland@asutherland.org>
2013-11-20 03:09:13 -0500

    Bug 883767 - [Email] Unsupported encoding in some email makes the email client explode. r=asuth +shepherd

--- test/unit/test_intl_unit.js
+++ test/unit/test_intl_unit.js
@@ -1,11 +1,11 @@
-define(['rdcommon/testcontext',
+define(['rdcommon/testcontext', './resources/th_main',
         'encoding', 'exports'],
-       function($tc, $encoding,
+       function($tc, $th_main, $encoding,
                 exports) {
 
 var TD = exports.TD = $tc.defineTestsFor(
   { id: 'test_intl_unit' }, null,
-  [], ['app']);
+  [$th_main.TESTHELPER], ['app']);
 
 TD.commonSimple('encoding aliases', function(eLazy) {
   var aliases = [
@@ -30,6 +30,45 @@ TD.commonSimple('encoding aliases', function(eLazy) {
     alias = aliases[i];
     eLazy.namedValue(alias[0], $encoding.checkEncoding(alias[0]));
   }
+});
+
+/**
+ * Run some encodings that actually exist but are unsupported by us /
+ * TextDecoder / the internet at large.  What we want to check here is:
+ * - That we don't throw an exception on seeing these things.
+ * - That we fall back to using utf-8 and see a correctly incorrect
+ *   interpretation of the encoded text.
+ */
+TD.commonCase('unsupported bad news encodings', function(T, RT) {
+  $th_main.thunkConsoleForNonTestUniverse();
+  var cases = [
+    {
+      name: 'iso-2022-cn (replacement encoder) (7-bit example)',
+      encoding: 'iso-2022-cn',
+      // from http://tools.ietf.org/html/rfc1922 1.2
+      input: '\x1b$)A\x0e=;;;\x1b$)GG(_P\x0f',
+      reallyEncodes: '\u4ea4\u6362\u4ea4\u63db',
+      // Decode it the same as the input since it's 7-bit and therefore won't
+      // result in non-ASCII or unicode replacement characters.
+      weWant: '\x1b$)A\x0e=;;;\x1b$)GG(_P\x0f',
+    },
+    {
+      name: 'gibberish encoding',
+      encoding: 'i-am-a-tomato',
+      input: 'TOMATO!',
+      reallyEncodes: null,
+      weWant: 'TOMATO!',
+    }
+  ];
+
+  var eCheck = T.lazyLogger('check');
+  cases.forEach(function(info) {
+    T.action(eCheck, info.name, function() {
+      eCheck.expect_namedValue('converted', info.weWant);
+      var outstr = $encoding.convert(info.input, 'utf-8', info.encoding);
+      eCheck.namedValue('converted', outstr);
+    });
+  });
 });
 
 }); // end define
