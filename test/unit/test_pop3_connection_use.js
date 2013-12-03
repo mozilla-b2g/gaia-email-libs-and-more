b8332aeda7a63b52c3c5d0f7f493e2b143586d3a
Marcus Cavanaugh <m@mcav.com>
2013-12-03 11:05:59 -0800

    Bug 945669 - [email/POP3] Make FolderStorage.sliceOpenMostRecent not trigger connection establishment for POP3 folders other than the inbox; clean up tests

---
+++ test/unit/test_pop3_connection_use.js
@@ -0,0 +1,56 @@
+/**
+ * Make sure POP3 only opens a connection for the inbox.
+ **/
+
+define(['rdcommon/testcontext', './resources/th_main',
+        'wbxml', 'activesync/codepages',
+        'exports'],
+       function($tc, $th_main, $wbxml, $ascp, exports) {
+
+var TD = exports.TD = $tc.defineTestsFor(
+  { id: 'test_pop3_connection_use' }, null,
+  [$th_main.TESTHELPER], ['app']);
+
+TD.commonCase('connection use', function(T, RT) {
+  T.group('setup');
+  var testUniverse = T.actor('testUniverse', 'U');
+  var testAccount = T.actor('testAccount', 'A', { universe: testUniverse });
+  var eSync = T.lazyLogger('sync');
+
+  var trashFolder = testAccount.do_useExistingFolderWithType('trash', '');
+  var inboxFolder = testAccount.do_useExistingFolderWithType('inbox', '');
+
+  T.action('close existing connection', function() {
+    testAccount.folderAccount._conn.disconnect();
+  });
+
+  // Trash must not create a connection.
+  testAccount.do_openFolderView(
+    'opens', trashFolder,
+    { count: 0, full: 0 },
+    { top: true, bottom: true, grow: false, newCount: null },
+    {
+      expectFunc: function() {
+        RT.reportActiveActorThisStep(testAccount.eFolderAccount);
+        testAccount.eFolderAccount.ignore_saveAccountState();
+      }
+    });
+
+  // Inbox must create a connection.
+  testAccount.do_openFolderView(
+    'opens', inboxFolder,
+    { count: 0, full: 0 },
+    { top: true, bottom: true, grow: false, newCount: null },
+    { syncedToDawnOfTime: true,
+      expectFunc: function() {
+        RT.reportActiveActorThisStep(testAccount.eFolderAccount);
+        testAccount.eFolderAccount.expect_createConnection();
+        testAccount.eFolderAccount.ignore_saveAccountState();
+      }
+    });
+
+
+  T.group('cleanup');
+});
+
+}); // end define
