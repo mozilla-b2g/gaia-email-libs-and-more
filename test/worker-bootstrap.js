4e862d63d7b0d11354f5507d392235736352704d
Marcus Cavanaugh <m@mcav.com>
2013-12-10 17:26:52 -0800

    Bug 938321 - [email/backend] Implement utf-7 charset support using hand-rolled decoder. r=asuth

--- test/worker-bootstrap.js
+++ test/worker-bootstrap.js
@@ -78,6 +78,7 @@ require({
     "event-queue": "data/lib/js-shims/event-queue",
     "microtime": "data/lib/js-shims/microtime",
     "path": "data/lib/js-shims/path",
+    "utf7": "data/lib/js-shims/utf7",
 
     "wbxml": "deps/activesync/wbxml/wbxml",
     "activesync": "deps/activesync",
