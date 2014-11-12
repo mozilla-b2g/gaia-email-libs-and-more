mozilla-b2g/gaia-email-libs-and-more

commit dd6d2d72943cd5b7add4a88c753c27af021be97c

Bug 1087184 - mozContacts API uses lowercase names for searches

js/mailapi.js
@@ -694,13 +694,24 @@ var ContactCache = exports.ContactCache = {
       this._contactCache[emailAddress] = null;
 
       this.pendingLookupCount++;
+
+      // Search contacts, but use an all lower-case name, since the contacts
+      // API's search plumbing uses a lowercase version of the email address
+      // for these search comparisons. However, the actual display of the
+      // contact in the contact app has casing preserved.
       var req = contactsAPI.find({
                   filterBy: ['email'],
                   filterOp: 'equals',
-                  filterValue: emailAddress
+                  filterValue: emailAddress.toLowerCase()
                 });
       var self = this, handleResult = function() {
         if (req.result && req.result.length) {
+          // CONSIDER TODO SOMEDAY: since the search is done witha a
+          // toLowerCase() call, it is conceivable that we could get multiple
+          // results with slightly different casing. It might be nice to try
+          // to find the best casing match, but the payoff for that is likely
+          // small, and the common case will be that the first one is good to
+          // use.
           var contact = req.result[0];
 
           ContactCache._contactCache[emailAddress] = contact;

commit 0118d64f5e14f5a54af9b5e2bfbdcce1f5e3a8f6

Bug 1087184: fix emailAddress toLower case issue when undefined, introduced in dd6d2d72943cd5b7add4a88c753c27af021be97c

js/mailapi.js
@@ -699,11 +699,19 @@ var ContactCache = exports.ContactCache = {
       // Search contacts, but use an all lower-case name, since the contacts
       // API's search plumbing uses a lowercase version of the email address
       // for these search comparisons. However, the actual display of the
-      // contact in the contact app has casing preserved.
+      // contact in the contact app has casing preserved. emailAddress could
+      // be undefined though if a group/undisclosed-recipients case, so guard
+      // against that (deeper normalization fix tracked in bug 1097820). Using
+      // empty string in the undefined emailAddress case because passing the
+      // value of undefined directly in the filterValue results in some contacts
+      // being returned. Potentially all contacts. However passing empty string
+      // gives back no results, even if there is a contact with no email address
+      // assigned to it.
+      var filterValue = emailAddress ? emailAddress.toLowerCase() : '';
       var req = contactsAPI.find({
                   filterBy: ['email'],
                   filterOp: 'equals',
-                  filterValue: emailAddress.toLowerCase()
+                  filterValue: filterValue
                 });
       var self = this, handleResult = function() {
         if (req.result && req.result.length) {